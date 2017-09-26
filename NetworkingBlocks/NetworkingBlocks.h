81735455af1b5239687a823831aa92900288aaab
Tayphoon <tayphoon.company@gmail.com>
2017-09-26 09:07:03 +0300

    Initial Commit

---
+++ NetworkingBlocks/NetworkingBlocks.h
@@ -0,0 +1,19 @@
+//
+//  NetworkingBlocks.h
+//  NetworkingBlocks
+//
+//  Created by Tayphoon on 26.09.17.
+//  Copyright © 2017 Tayphoon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkingBlocks.
+FOUNDATION_EXPORT double NetworkingBlocksVersionNumber;
+
+//! Project version string for NetworkingBlocks.
+FOUNDATION_EXPORT const unsigned char NetworkingBlocksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingBlocks/PublicHeader.h>
+
+
---
+++ NetworkingBlocksTests/NetworkingBlocksTests.m
@@ -0,0 +1,39 @@
+//
+//  NetworkingBlocksTests.m
+//  NetworkingBlocksTests
+//
+//  Created by Tayphoon on 26.09.17.
+//  Copyright © 2017 Tayphoon. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NetworkingBlocksTests : XCTestCase
+
+@end
+
+@implementation NetworkingBlocksTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
